f2791bb74ce80c91fc1c18d6d940d02f72325694
SyndicateCorp <riyatabdul11@gmail.com>
2023-10-09 11:39:53 +0700

    minggu5
    minggu5(TT5)

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,17 @@
+import { Component, Renderer2, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor(private renderer: Renderer2) {
+    this.renderer.addClass(document.body, "register-page");
+  }
+  
+  ngOnInit(): void {
+    
+  }
+}
