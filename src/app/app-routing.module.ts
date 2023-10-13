7155cc725186a87f65fc7d6074f9065e365fd105
SyndicateCorp <riyatabdul11@gmail.com>
2023-10-13 10:09:13 +0700

    Minggu6
    minggu6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "login", pathMatch: "full" },
-  { path: "dashboard", component: DashboardComponent },
+  { path: "admin", component: AdminComponent },
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent}
 ];
