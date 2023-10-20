e9f449ab911882de0810f46677b6821aed54965e
SyndicateCorp <riyatabdul11@gmail.com>
2023-10-20 11:12:38 +0700

    minggu7
    minggu7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,21 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AdminComponent } from './admin/admin.component';
+import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { Dashboard3Component } from './dashboard3/dashboard3.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "login", pathMatch: "full" },
-  { path: "admin", component: AdminComponent },
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent}
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'admin', component: AdminComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard2', component: Dashboard2Component },
+  { path: 'dashboard3', component: Dashboard3Component },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
