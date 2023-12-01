01790ffbac7fb52ff1a044a36515778e39c84e48
SyndicateCorp <riyatabdul11@gmail.com>
2023-12-01 11:23:53 +0700

    minggu12
    logIn - LogOut

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,7 +7,19 @@ import { Component, Input} from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+    constructor(
+    private router: Router,
+  ) {
+  }
 
   @Input() moduleName: string = "";
+
+  signOut(): void {
+    sessionStorage.clear();
+
+    console.log('Session data berhasil dihapus');
+
+    this.router.navigate(['/login']);
+  };
 
 }
